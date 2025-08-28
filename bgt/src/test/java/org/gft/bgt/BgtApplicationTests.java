2d3e903606aba842ff90b8e4e1ddce2c42c84715
carlos.luna <57882278+carlosLunaGarca@users.noreply.github.com>
2025-08-28 10:34:56 -0500

    init

---
+++ bgt/src/test/java/org/gft/bgt/BgtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gft.bgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
